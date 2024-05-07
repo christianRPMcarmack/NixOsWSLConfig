f2fa56a58fcff4dd18f878ea708094dfe108e3a2
System administrator <root@nixos.>
2024-05-07 03:39:52 +0000

    first commit

---
+++ configuration.nix
@@ -0,0 +1,77 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    # Include NixOS-WSL modules
+	    <nixos-wsl/modules>
+  ];
+
+  wsl.enable = true;
+  wsl.defaultUser = "ccarmack";
+
+  system.stateVersion = "23.11";
+  
+  nix.extraOptions = ''experimental-features = nix-command flakes'';
+
+  # Add build inputs directly to the environment
+  environment.systemPackages = with pkgs; [
+    openssl
+    pkg-config
+    eza
+    fd
+    neovim
+    xclip
+    git
+    wget
+    home-manager
+    openssl
+    htop
+    pkg-config
+    ripgrep
+    lazygit
+    bat
+    nh
+    dust
+  ];
+  
+  networking.firewall = {
+    enable = true;
+    #allowedTCPPorts = [ 80 443 ];
+    #allowedUDPPortRanges = [
+    #  { from = 4000; to = 4007; }
+    #  { from = 8000; to = 8010; }
+    #];
+  };
+
+  # Define shell aliases directly in the user environment
+  environment.shellAliases = {
+    ls = "eza";
+    find = "fd";
+    lg = "lazygit";
+    cat = "bat -p";
+  };
+
+  programs.git.config = {
+    user.name = "ccarmack";
+    user.email = "christian.carmack@outlook.com";
+  };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+  };
+ 
+  users.defaultUserShell = pkgs.zsh;
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    ohMyZsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = "bira";
+    };
+  };
+
+}
